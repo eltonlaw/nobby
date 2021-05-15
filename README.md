676490085bc8023e62edd728fd4cf95d6d48c124
Elton Law <eltonlaw296@gmail.com>
2021-05-15 14:21:05 -0400

    [CMAKE] get version from cmake config, init nobbyd

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 ## Testing
 
+Run unit tests
+
     cmake . && make && CTEST_OUTPUT_ON_FAILURE=1 make test
+
+Run CLI
+
+    cmake . && make && ./nobby/nobbyd
---
+++ nobby/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "config.h"
+
+int main() {
+    std::cout << "nobby version" << NOBBY_VERSION_MAJOR << "."<< NOBBY_VERSION_MINOR << std::endl;
+    return 0;
+}
